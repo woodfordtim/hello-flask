3df4fe7c6a3357d77d420d0fa760aee02a770243
woodfordtim <woodfordtim@gmail.com>
2020-02-19 15:38:44 +0000

    routing and templates: POST lesson

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 import os
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = 'some_secret'
 
 @app.route("/")
 def index():
@@ -9,15 +11,33 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title
+        ="About", company=data)
 
-@app.route("/contact")
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+
+    return render_template("member.html", member=member)
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST"
+        print(request.form)
+    return render_template("contact.html", page_title="Contact")
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
